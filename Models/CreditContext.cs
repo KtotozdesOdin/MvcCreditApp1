b44c51f52b69a881c635ed305c36ea1fcd0bc541
KtotozdesOdin <se.me.nov007@gmail.com>
2025-04-01 22:46:57 +0300

    Add project files.

---
+++ Models/CreditContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCreditApp1.Models;
+
+namespace MvcCreditApp.Data
+{
+    public class CreditContext : DbContext
+    {
+        public CreditContext(DbContextOptions<CreditContext> options)
+    : base(options)
+        { }
+        public DbSet<Credit> Credits { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+    }
+}
