4e07e30b6dbab04c6a5871113a46b279af500e44
KtotozdesOdin <se.me.nov007@gmail.com>
2025-04-05 10:57:30 +0300

    lab6_ex2 Применение аннотации данных в модели

--- Models/Credit.cs
+++ Models/Credit.cs
@@ -1,16 +1,27 @@
-﻿namespace MvcCreditApp1.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCreditApp1.Models
 {
     public class Credit
     {
         // ID кредита 
         public virtual int CreditId { get; set; }
         // Название 
+        [DisplayName("Название кредита")]
+        [Required]
         public virtual string Head { get; set; }
         // Период, на который выдается кредит 
+        [DisplayName("Срок кредитования")]
+        [Required]
         public virtual int Period { get; set; }
         // Максимальная сумма кредита 
+        [DisplayName("Максимальная сумма кредита")]
+        [Required]
         public virtual int Sum { get; set; }
         // Процентная ставка 
+        [DisplayName("Процентная ставка")]
+        [Required]
         public virtual int Procent { get; set; }
     }
 }
